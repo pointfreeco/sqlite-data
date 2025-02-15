78383392052525564bb306754c867156460d0666
Brandon Williams <mbrandonw@hey.com>
2025-02-15 14:03:30 -0800

    Update tests.

--- Tests/SharingGRDBTests/SharingGRDBTests.swift
+++ Tests/SharingGRDBTests/SharingGRDBTests.swift
@@ -4,14 +4,16 @@ import Sharing
 import SharingGRDB
 import Testing
 
-struct GRDBSharingTests {
+@Suite struct GRDBSharingTests {
   @Test
   func fetchOne() async throws {
-    try withDependencies {
+    try await withDependencies {
       $0.defaultDatabase = try DatabaseQueue()
     } operation: {
       @SharedReader(.fetchOne(sql: "SELECT 1")) var bool = false
+      try await Task.sleep(nanoseconds: 10_000_000)
       #expect(bool)
+      #expect($bool.loadError == nil)
     }
   }
 
@@ -22,6 +24,19 @@ struct GRDBSharingTests {
     } operation: {
       @SharedReader(.fetchOne(sql: "SELECT NULL")) var bool: Bool?
       #expect(bool == nil)
+    }
+  }
+
+  @Test func fetchSyntaxError() async throws {
+    try await withDependencies {
+      $0.defaultDatabase = try DatabaseQueue()
+    } operation: {
+      @SharedReader(.fetchOne(sql: "SELEC 1")) var bool = false
+      #expect(bool == false)
+      try await Task.sleep(nanoseconds: 10_000_000)
+      #expect($bool.loadError is DatabaseError?)
+      let error = try #require($bool.loadError as? DatabaseError)
+      #expect(error.message == #"near "SELEC": syntax error"#)
     }
   }
 }
