6a6c1ff5363ce90c44a7e5b642c9d4310f0f82e5
Brandon Williams <mbrandonw@hey.com>
2025-03-20 08:47:53 -0500

    wip

--- Examples/Reminders/RemindersLists.swift
+++ Examples/Reminders/RemindersLists.swift
@@ -8,15 +8,15 @@ import SwiftUI
 struct RemindersListsView: View {
   @SharedReader(
     .fetchAll(
-    RemindersList
-      .group(by: \.id)
-      .leftJoin(Reminder.all()) { $0.id.eq($1.remindersListID) }
-      .select {
-        ReminderListState.Columns(
-          reminderCount: #sql("total(NOT \($1.isCompleted))"),
-          remindersList: $0
-        )
-      },
+      RemindersList
+        .group(by: \.id)
+        .leftJoin(Reminder.all()) { $0.id.eq($1.remindersListID) }
+        .select {
+          ReminderListState.Columns(
+            reminderCount: #sql("total(NOT \($1.isCompleted))"),
+            remindersList: $0
+          )
+        },
       animation: .default
     )
   )
@@ -72,7 +72,7 @@ struct RemindersListsView: View {
           }
         }
         .buttonStyle(.plain)
-        
+
         Section {
           ForEach(remindersLists) { state in
             NavigationLink {
