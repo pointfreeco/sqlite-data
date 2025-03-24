d411fc5415370007bfa8d4e86369d2d5ce1d6304
Stephen Celis <stephen@stephencelis.com>
2025-03-24 13:22:12 -0700

    wip

--- Sources/StructuredQueriesGRDBCore/QueryCursor.swift
+++ Sources/StructuredQueriesGRDBCore/QueryCursor.swift
@@ -1,3 +1,4 @@
+import Foundation
 import GRDB
 import SQLite3
 import StructuredQueriesCore
@@ -18,28 +19,15 @@ public final class QueryCursor<each QueryValue: QueryRepresentable>: DatabaseCur
     let query = query.query
     guard !query.isEmpty else { throw EmptyQuery() }
     _statement = try db.makeStatement(sql: query.string)
+    _statement.arguments = StatementArguments(query.bindings.map(\.databaseValue))
     decoder = SQLiteQueryDecoder(
       database: db.sqliteConnection,
       statement: _statement.sqliteStatement
     )
-    for (index, binding) in zip(Int32(1)..., query.bindings) {
-      let result =
-        switch binding {
-        case let .blob(blob):
-          sqlite3_bind_blob(
-            _statement.sqliteStatement, index, Array(blob), Int32(blob.count), SQLITE_TRANSIENT
-          )
-        case let .double(double):
-          sqlite3_bind_double(_statement.sqliteStatement, index, double)
-        case let .int(int):
-          sqlite3_bind_int64(_statement.sqliteStatement, index, Int64(int))
-        case .null:
-          sqlite3_bind_null(_statement.sqliteStatement, index)
-        case let .text(text):
-          sqlite3_bind_text(_statement.sqliteStatement, index, text, -1, SQLITE_TRANSIENT)
-        }
-      guard result == SQLITE_OK else { throw SQLiteError(db.sqliteConnection) }
-    }
+  }
+
+  deinit {
+    sqlite3_reset(_statement.sqliteStatement)
   }
 
   public func _element(sqliteStatement _: SQLiteStatement) throws -> Element {
@@ -53,10 +41,19 @@ public final class QueryCursor<each QueryValue: QueryRepresentable>: DatabaseCur
 
 private let SQLITE_TRANSIENT = unsafeBitCast(-1, to: sqlite3_destructor_type.self)
 
-struct SQLiteError: Error {
-  let message: String
-
-  init(_ handle: OpaquePointer?) {
-    self.message = String(cString: sqlite3_errmsg(handle))
+extension QueryBinding {
+  fileprivate var databaseValue: DatabaseValue {
+    switch self {
+    case let .blob(blob):
+      return Data(blob).databaseValue
+    case let .double(double):
+      return double.databaseValue
+    case let .int(int):
+      return int.databaseValue
+    case .null:
+      return .null
+    case let .text(text):
+      return text.databaseValue
+    }
   }
 }
