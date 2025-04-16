ae3cdc393d4e53eeb3dbd7a9bc0e786ec8bce7ca
Stephen Celis <stephen@stephencelis.com>
2025-04-16 14:39:40 -0700

    fix

--- Tests/SharingGRDBTests/SharingGRDBTests.swift
+++ Tests/SharingGRDBTests/SharingGRDBTests.swift
@@ -11,7 +11,7 @@ import Testing
       $0.defaultDatabase = try DatabaseQueue()
     } operation: {
       @SharedReader(.fetchOne(sql: "SELECT 1")) var bool = false
-      try await Task.sleep(nanoseconds: 10_000_000)
+      try await Task.sleep(nanoseconds: 100_000_000)
       #expect(bool)
       #expect($bool.loadError == nil)
     }
@@ -33,7 +33,7 @@ import Testing
     } operation: {
       @SharedReader(.fetchOne(sql: "SELEC 1")) var bool = false
       #expect(bool == false)
-      try await Task.sleep(nanoseconds: 10_000_000)
+      try await Task.sleep(nanoseconds: 100_000_000)
       #expect($bool.loadError is DatabaseError?)
       let error = try #require($bool.loadError as? DatabaseError)
       #expect(error.message == #"near "SELEC": syntax error"#)
@@ -41,15 +41,16 @@ import Testing
   }
 
   @Test func fetchWithTwoDatabaseConnections() async throws {
+    let name = #function
     try await withDependencies {
-      $0.defaultDatabase = try .database
+      $0.defaultDatabase = try .database(named: name)
     } operation: {
       @SharedReader(.fetchAll(sql: "SELECT * FROM records")) var records1: [Record] = []
       try await Task.sleep(nanoseconds: 100_000_000)
       #expect(records1.map(\.id) == [1, 2, 3])
 
       try await withDependencies {
-        $0.defaultDatabase = try .database
+        $0.defaultDatabase = try .database(named: name)
       } operation: {
         @Dependency(\.defaultDatabase) var database2
         @SharedReader(.fetchAll(sql: "SELECT * FROM records")) var records2: [Record] = []
@@ -68,14 +69,14 @@ import Testing
     }
   }
 
-  @Test(.dependency(\.defaultDatabase, try .database))
+  @Test(.dependency(\.defaultDatabase, try .database()))
   func fetchIDHashValue() async throws {
     let fetchKey1: some SharedReaderKey<Void> = .fetch(Fetch1())
     let fetchKey2: some SharedReaderKey<Void> = .fetch(Fetch2())
     #expect(fetchKey1.id.hashValue != fetchKey2.id.hashValue)
   }
 
-  @Test(.dependency(\.defaultDatabase, try .database))
+  @Test(.dependency(\.defaultDatabase, try .database()))
   func fetchAnimationHashValue() async throws {
     let fetchKey1: some SharedReaderKey<Void> = .fetch(Fetch1())
     let fetchKey2: some SharedReaderKey<Void> = .fetch(Fetch2(), animation: .default)
@@ -97,20 +98,23 @@ fileprivate struct Record: Codable, Equatable, FetchableRecord, MutablePersistab
   let id: Int
 }
 extension DatabaseWriter where Self == DatabaseQueue {
-  fileprivate static var database: DatabaseQueue {
-    get throws {
-      let database = try DatabaseQueue(named: "db")
-      var migrator = DatabaseMigrator()
-      migrator.registerMigration("Up") { db in
-        try db.create(table: "records") { table in
-          table.column("id", .integer).primaryKey(autoincrement: true)
-        }
-        for index in 1...3 {
-          _ = try Record(id: index).inserted(db)
-        }
-      }
-      try migrator.migrate(database)
-      return database
+  fileprivate static func database(named name: String? = nil) throws -> DatabaseQueue {
+    let database: DatabaseQueue
+    if let name {
+      database = try DatabaseQueue(named: name)
+    } else {
+      database = try DatabaseQueue()
     }
+    var migrator = DatabaseMigrator()
+    migrator.registerMigration("Up") { db in
+      try db.create(table: "records") { table in
+        table.column("id", .integer).primaryKey(autoincrement: true)
+      }
+      for index in 1...3 {
+        _ = try Record(id: index).inserted(db)
+      }
+    }
+    try migrator.migrate(database)
+    return database
   }
 }
