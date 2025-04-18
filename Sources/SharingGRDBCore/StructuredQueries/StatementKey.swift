491d6304e50d315bc9b077af7fef518521fb831b
Stephen Celis <stephen@stephencelis.com>
2025-04-18 15:31:50 -0700

    Add fetchOne overloads for SelectStatement

--- Sources/SharingGRDBCore/StructuredQueries/StatementKey.swift
+++ Sources/SharingGRDBCore/StructuredQueries/StatementKey.swift
@@ -63,6 +63,28 @@ extension SharedReaderKey {
   ///
   /// This key takes a query built using the StructuredQueries library.
   ///
+  /// - Parameters:
+  ///   - statement: A structured query describing the data to be fetched.
+  ///   - database: The database to read from. A value of `nil` will use the default database
+  ///     (`@Dependency(\.defaultDatabase)`).
+  /// - Returns: A key that can be passed to the `@SharedReader` property wrapper.
+  public static func fetchOne<S: SelectStatement>(
+    _ statement: S,
+    database: (any DatabaseReader)? = nil
+  ) -> Self
+  where
+    S.QueryValue == (),
+    S.Joins == (),
+    Self == FetchKey<S.From.QueryOutput>
+  {
+    let statement = statement.selectStar()
+    return fetchOne(statement, database: database)
+  }
+
+  /// A key that can query for a value in a SQLite database.
+  ///
+  /// This key takes a query built using the StructuredQueries library.
+  ///
   /// ```swift
   /// @SharedReader(.fetchOne(Item.count())) var itemCount = 0
   /// ```
@@ -145,6 +167,32 @@ extension SharedReaderKey {
   /// This key takes a query built using the StructuredQueries library.
   ///
   /// ```swift
+  /// @SharedReader(.fetchAll(Item.order(by: \.name))) var items
+  /// ```
+  ///
+  /// - Parameters:
+  ///   - statement: A structured query describing the data to be fetched.
+  ///   - database: The database to read from. A value of `nil` will use the default database
+  ///     (`@Dependency(\.defaultDatabase)`).
+  /// - Returns: A key that can be passed to the `@SharedReader` property wrapper.
+  @_disfavoredOverload
+  public static func fetchOne<S: SelectStatement, each J: StructuredQueriesCore.Table>(
+    _ statement: S,
+    database: (any DatabaseReader)? = nil
+  ) -> Self
+  where
+    S.QueryValue == (),
+    S.Joins == (repeat each J),
+    Self == FetchKey<(S.From.QueryOutput, repeat (each J).QueryOutput)>
+  {
+    fetchOne(statement.selectStar(), database: database)
+  }
+
+  /// A key that can query for a value in a SQLite database.
+  ///
+  /// This key takes a query built using the StructuredQueries library.
+  ///
+  /// ```swift
   /// @SharedReader(.fetchOne(Item.count())) var itemCount = 0
   /// ```
   ///
@@ -211,6 +259,31 @@ extension SharedReaderKey {
     fetch(
       FetchAllStatementValueRequest(statement: statement), database: database, scheduler: scheduler
     )
+  }
+
+  /// A key that can query for a value in a SQLite database.
+  ///
+  /// A version of `fetchOne` that can be configured with a scheduler.
+  ///
+  /// - Parameters:
+  ///   - statement: A structured query describing the data to be fetched.
+  ///   - database: The database to read from. A value of `nil` will use the default database
+  ///     (`@Dependency(\.defaultDatabase)`).
+  ///   - scheduler: The scheduler to observe from. By default, database observation is performed
+  ///     asynchronously on the main queue.
+  /// - Returns: A key that can be passed to the `@SharedReader` property wrapper.
+  public static func fetchOne<S: SelectStatement>(
+    _ statement: S,
+    database: (any DatabaseReader)? = nil,
+    scheduler: some ValueObservationScheduler & Hashable
+  ) -> Self
+  where
+    S.QueryValue == (),
+    S.Joins == (),
+    Self == FetchKey<S.From.QueryOutput>
+  {
+    let statement = statement.selectStar()
+    return fetchOne(statement, database: database, scheduler: scheduler)
   }
 
   /// A key that can query for a value in a SQLite database.
@@ -310,6 +383,32 @@ extension SharedReaderKey {
   /// - Returns: A key that can be passed to the `@SharedReader` property wrapper.
   @_disfavoredOverload
   @_documentation(visibility: private)
+  public static func fetchOne<S: SelectStatement, each J: StructuredQueriesCore.Table>(
+    _ statement: S,
+    database: (any DatabaseReader)? = nil,
+    scheduler: some ValueObservationScheduler & Hashable
+  ) -> Self
+  where
+    S.QueryValue == (),
+    S.Joins == (repeat each J),
+    Self == FetchKey<(S.From.QueryOutput, repeat (each J).QueryOutput)>
+  {
+    fetchOne(statement.selectStar(), database: database, scheduler: scheduler)
+  }
+
+  /// A key that can query for a value in a SQLite database.
+  ///
+  /// A version of `fetchOne` that can be configured with a scheduler.
+  ///
+  /// - Parameters:
+  ///   - statement: A structured query describing the data to be fetched.
+  ///   - database: The database to read from. A value of `nil` will use the default database
+  ///     (`@Dependency(\.defaultDatabase)`).
+  ///   - scheduler: The scheduler to observe from. By default, database observation is performed
+  ///     asynchronously on the main queue.
+  /// - Returns: A key that can be passed to the `@SharedReader` property wrapper.
+  @_disfavoredOverload
+  @_documentation(visibility: private)
   public static func fetchOne<each Value: QueryRepresentable>(
     _ statement: some StructuredQueriesCore.Statement<(repeat each Value)>,
     database: (any DatabaseReader)? = nil,
@@ -373,6 +472,31 @@ extension SharedReaderKey {
         database: database,
         animation: animation
       )
+    }
+
+    /// A key that can query for a value in a SQLite database.
+    ///
+    /// A version of `fetchOne` that can be configured with a SwiftUI animation.
+    ///
+    /// - Parameters:
+    ///   - statement: A structured query describing the data to be fetched.
+    ///   - database: The database to read from. A value of `nil` will use the default database
+    ///     (`@Dependency(\.defaultDatabase)`).
+    ///   - animation: The animation to use for user interface changes that result from changes to
+    ///     the fetched results.
+    /// - Returns: A key that can be passed to the `@SharedReader` property wrapper.
+    public static func fetchOne<S: SelectStatement>(
+      _ statement: S,
+      database: (any DatabaseReader)? = nil,
+      animation: Animation
+    ) -> Self
+    where
+      S.QueryValue == (),
+      S.Joins == (),
+      Self == FetchKey<S.From.QueryOutput>
+    {
+      let statement = statement.selectStar()
+      return fetchOne(statement, database: database, animation: animation)
     }
 
     /// A key that can query for a collection of value in a SQLite database.
@@ -456,6 +580,33 @@ extension SharedReaderKey {
         database: database,
         animation: animation
       )
+    }
+
+    /// A key that can query for a value in a SQLite database.
+    ///
+    /// A version of ``Sharing/SharedReaderKey/fetchOne(_:database:)`` that can be configured with a
+    /// SwiftUI animation.
+    ///
+    /// - Parameters:
+    ///   - statement: A structured query describing the data to be fetched.
+    ///   - database: The database to read from. A value of `nil` will use the default database
+    ///     (`@Dependency(\.defaultDatabase)`).
+    ///   - animation: The animation to use for user interface changes that result from changes to
+    ///     the fetched results.
+    /// - Returns: A key that can be passed to the `@SharedReader` property wrapper.
+    @_disfavoredOverload
+    @_documentation(visibility: private)
+    public static func fetchOne<S: SelectStatement, each J: StructuredQueriesCore.Table>(
+      _ statement: S,
+      database: (any DatabaseReader)? = nil,
+      animation: Animation
+    ) -> Self
+    where
+      S.QueryValue == (),
+      S.Joins == (repeat each J),
+      Self == FetchKey<(S.From.QueryOutput, repeat (each J).QueryOutput)>
+    {
+      fetchOne(statement.selectStar(), database: database, animation: animation)
     }
 
     /// A key that can query for a value in a SQLite database.
