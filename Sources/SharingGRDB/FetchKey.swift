6e9336c51f27f6d13039c9c9bb09863ef2bc350d
Stephen Celis <stephen@stephencelis.com>
2025-04-17 17:14:51 -0700

    wip

--- Sources/SharingGRDB/FetchKey.swift
+++ Sources/SharingGRDB/FetchKey.swift
@@ -401,18 +401,3 @@ private struct FetchOne<Value: DatabaseValueConvertible>: FetchKeyRequest {
 public struct NotFound: Error {
   public init() {}
 }
-
-
-import SwiftData
-@available(iOS 17, *)
-@Model
-class Reminder {
-  var title = ""
-  var isCompleted = false
-  var notes = ""
-  init(title: String = "", isCompleted: Bool = false, notes: String = "") {
-    self.title = title
-    self.isCompleted = isCompleted
-    self.notes = notes
-  }
-}
