a8235fca3bf7bad910b3ef1e596bbb93f08fbf89
Brandon Williams <135203+mbrandonw@users.noreply.github.com>
2025-04-22 17:24:50 -0700

    Disfavor an init to allow for bare @FetchAll syntax (#39)
    * more tests
    
    * fix test

---
+++ Tests/SharingGRDBTests/FetchTests.swift
@@ -0,0 +1,53 @@
+import Dependencies
+import DependenciesTestSupport
+import GRDB
+import Sharing
+import SharingGRDB
+import StructuredQueries
+import SwiftUI
+import Testing
+
+@Suite(.dependency(\.defaultDatabase, try .database()))
+struct FetchTests {
+  @Test func bareFetchAll() async throws {
+    @FetchAll var records: [Record]
+    try await Task.sleep(nanoseconds: 100_000_000)
+    #expect(records == [Record(id: 1), Record(id: 2), Record(id: 3)])
+  }
+
+  @Test func fetchAllWithQuery() async throws {
+    @FetchAll(Record.where { $0.id > 1 }) var records: [Record]
+    try await Task.sleep(nanoseconds: 100_000_000)
+    #expect(records == [Record(id: 2), Record(id: 3)])
+  }
+
+  @Test func fetchCountWithQuery() async throws {
+    @FetchOne(Record.where { $0.id > 1 }.count()) var recordsCount = 0
+    try await Task.sleep(nanoseconds: 100_000_000)
+    #expect(recordsCount == 2)
+  }
+}
+
+@Table
+private struct Record: Equatable {
+  let id: Int
+}
+extension DatabaseWriter where Self == DatabaseQueue {
+  fileprivate static func database() throws -> DatabaseQueue {
+    let database = try DatabaseQueue()
+    var migrator = DatabaseMigrator()
+    migrator.registerMigration("Up") { db in
+      try #sql(
+        """
+        CREATE TABLE "records" ("id" INTEGER PRIMARY KEY AUTOINCREMENT)
+        """
+      )
+      .execute(db)
+      for _ in 1...3 {
+        _ = try Record.insert(Record.Draft()).execute(db)
+      }
+    }
+    try migrator.migrate(database)
+    return database
+  }
+}
