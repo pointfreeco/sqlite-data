1dfdaddd9ba0103731dafcd750ae11434eba143c
Brandon Williams <mbrandonw@hey.com>
2025-05-23 08:13:36 -0700

    revert this later

--- Examples/Reminders/RemindersApp.swift
+++ Examples/Reminders/RemindersApp.swift
@@ -4,32 +4,16 @@ import SwiftUI
 
 @main
 struct RemindersApp: App {
-  @Dependency(\.context) var context
-
   init() {
-    if context == .live {
-      try! prepareDependencies {
-        $0.defaultDatabase = try Reminders.appDatabase()
-        $0.defaultSyncEngine = SyncEngine(
-          container: CKContainer(identifier: "iCloud.co.pointfree.sharing-grdb.Reminders"),
-          database: $0.defaultDatabase,
-          tables: [
-            RemindersList.self,
-            Reminder.self,
-            Tag.self,
-            ReminderTag.self,
-          ]
-        )
-      }
+    try! prepareDependencies {
+      $0.defaultDatabase = try Reminders.appDatabase()
     }
   }
 
   var body: some Scene {
     WindowGroup {
-      if context == .live {
-        NavigationStack {
-          RemindersListsView()
-        }
+      NavigationStack {
+        RemindersListsView()
       }
     }
   }
