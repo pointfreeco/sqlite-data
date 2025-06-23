20a52078b50bc1dfa80d56cd30765de56f664a4a
Brandon Williams <mbrandonw@hey.com>
2025-06-23 16:21:17 -0700

    wip

--- Examples/CloudKitDemo/Schema.swift
+++ Examples/CloudKitDemo/Schema.swift
@@ -12,7 +12,7 @@ func appDatabase() throws -> any DatabaseWriter {
   var configuration = Configuration()
   configuration.foreignKeysEnabled = false
   configuration.prepareDatabase { db in
-    db.attachMetadatabase(containerIdentifier: "iCloud.co.pointfree.SQLiteData.demos.CloudKitDemo")
+    try db.attachMetadatabase(containerIdentifier: "iCloud.co.pointfree.SQLiteData.demos.CloudKitDemo")
     db.trace {
       print($0.expandedDescription)
     }
