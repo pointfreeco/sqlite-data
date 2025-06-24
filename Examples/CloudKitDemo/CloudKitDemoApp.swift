80268867541b8c6b929858c1df0ced478b33ef19
Brandon Williams <mbrandonw@hey.com>
2025-06-24 15:13:55 -0700

    wip;

--- Examples/CloudKitDemo/CloudKitDemoApp.swift
+++ Examples/CloudKitDemo/CloudKitDemoApp.swift
@@ -1,14 +1,15 @@
 import CloudKit
 import SharingGRDB
 import SwiftUI
+
 #if canImport
-import UIKit
+  import UIKit
 #endif
 
 @main
 struct CloudKitDemoApp: App {
-#if canImport(UIKit)
-  @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+  #if canImport(UIKit)
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
   #endif
   var body: some Scene {
     WindowGroup {
@@ -20,32 +21,33 @@ struct CloudKitDemoApp: App {
 }
 
 #if canImport(UIKit)
-class AppDelegate: UIResponder, UIApplicationDelegate, ObservableObject {
-  func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
-  ) -> Bool {
-    try! prepareDependencies {
-      $0.defaultDatabase = try appDatabase()
+  class AppDelegate: UIResponder, UIApplicationDelegate, ObservableObject {
+    func application(
+      _ application: UIApplication,
+      didFinishLaunchingWithOptions launchOptions: [UIApplication
+        .LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+      try! prepareDependencies {
+        $0.defaultDatabase = try appDatabase()
+      }
+      return true
     }
-    return true
+
+    func application(
+      _ application: UIApplication,
+      configurationForConnecting connectingSceneSession: UISceneSession,
+      options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+      let configuration = UISceneConfiguration(
+        name: "Default Configuration",
+        sessionRole: connectingSceneSession.role
+      )
+      configuration.delegateClass = SceneDelegate.self
+      return configuration
+    }
   }
 
-  func application(
-    _ application: UIApplication,
-    configurationForConnecting connectingSceneSession: UISceneSession,
-    options: UIScene.ConnectionOptions
-  ) -> UISceneConfiguration {
-    let configuration = UISceneConfiguration(
-      name: "Default Configuration",
-      sessionRole: connectingSceneSession.role
-    )
-    configuration.delegateClass = SceneDelegate.self
-    return configuration
+  class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
   }
-}
-
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  var window: UIWindow?
-}
 #endif
