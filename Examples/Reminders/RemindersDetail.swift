c69b68030ca36507a23c03f413142071bf7dfe17
Sean Woodward <sean@wwwoodward.com>
2025-07-06 10:25:30 -0500

    fix #91

--- Examples/Reminders/RemindersDetail.swift
+++ Examples/Reminders/RemindersDetail.swift
@@ -85,7 +85,7 @@ class RemindersDetailModel: HashableObject {
       .withTags
       .where { reminder, _, tag in
         switch detailType {
-        case .all: !reminder.isCompleted
+        case .all: if false { SQLQueryExpression("") } // break will not compile
         case .completed: reminder.isCompleted
         case .flagged: reminder.isFlagged
         case .remindersList(let list): reminder.remindersListID.eq(list.id)
