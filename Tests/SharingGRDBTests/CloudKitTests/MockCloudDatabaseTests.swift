98d57a1f72ef3deab005df64b7a007cf159c858b
Brandon Williams <mbrandonw@hey.com>
2025-07-12 09:44:37 -0700

    wip

--- Tests/SharingGRDBTests/CloudKitTests/MockCloudDatabaseTests.swift
+++ Tests/SharingGRDBTests/CloudKitTests/MockCloudDatabaseTests.swift
@@ -50,6 +50,30 @@ extension BaseCloudKitTests {
     }
 
     @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
+    @Test func saveTransaction_ChildNoParent() async throws {
+      let parent = CKRecord(recordType: "A", recordID: CKRecord.ID(recordName: "A"))
+      let child = CKRecord(recordType: "B", recordID: CKRecord.ID(recordName: "B"))
+      child.parent = CKRecord.Reference(record: parent, action: .none)
+
+      await syncEngine.modifyRecords(scope: .private, saving: [child])
+
+      assertInlineSnapshot(of: syncEngine.container, as: .customDump) {
+        """
+        MockCloudContainer(
+          privateCloudDatabase: MockCloudDatabase(
+            databaseScope: .private,
+            storage: []
+          ),
+          sharedCloudDatabase: MockCloudDatabase(
+            databaseScope: .shared,
+            storage: []
+          )
+        )
+        """
+      }
+    }
+
+    @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
     @Test func deleteTransaction_ParentBeforeChild() async throws {
       let parent = CKRecord(recordType: "A", recordID: CKRecord.ID(recordName: "A"))
       let child = CKRecord(recordType: "B", recordID: CKRecord.ID(recordName: "B"))
@@ -64,6 +88,44 @@ extension BaseCloudKitTests {
           privateCloudDatabase: MockCloudDatabase(
             databaseScope: .private,
             storage: []
+          ),
+          sharedCloudDatabase: MockCloudDatabase(
+            databaseScope: .shared,
+            storage: []
+          )
+        )
+        """
+      }
+    }
+
+    @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
+    @Test func deleteTransaction_DeleteParentButNotChild() async throws {
+      let parent = CKRecord(recordType: "A", recordID: CKRecord.ID(recordName: "A"))
+      let child = CKRecord(recordType: "B", recordID: CKRecord.ID(recordName: "B"))
+      child.parent = CKRecord.Reference(record: parent, action: .none)
+
+      await syncEngine.modifyRecords(scope: .private, saving: [child, parent])
+      await syncEngine.modifyRecords(scope: .private, deleting: [parent.recordID])
+
+      assertInlineSnapshot(of: syncEngine.container, as: .customDump) {
+        """
+        MockCloudContainer(
+          privateCloudDatabase: MockCloudDatabase(
+            databaseScope: .private,
+            storage: [
+              [0]: CKRecord(
+                recordID: CKRecord.ID(A/_defaultZone/__defaultOwner__),
+                recordType: "A",
+                parent: nil,
+                share: nil
+              ),
+              [1]: CKRecord(
+                recordID: CKRecord.ID(B/_defaultZone/__defaultOwner__),
+                recordType: "B",
+                parent: CKReference(recordID: CKRecord.ID(A/_defaultZone/__defaultOwner__)),
+                share: nil
+              )
+            ]
           ),
           sharedCloudDatabase: MockCloudDatabase(
             databaseScope: .shared,
