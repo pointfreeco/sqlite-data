6519f176192bee69f4445c60e1e55ddc497401b8
Brandon Williams <mbrandonw@hey.com>
2025-07-30 08:18:40 -0500

    Public method for fetching metadata.

--- Sources/SharingGRDBCore/CloudKit/SyncMetadata.swift
+++ Sources/SharingGRDBCore/CloudKit/SyncMetadata.swift
@@ -1,149 +1,158 @@
 #if canImport(CloudKit)
-import CloudKit
+  import CloudKit
 
-/// A table that tracks metadata related to synchronized data.
-///
-/// Each row of this table represents a synchronized record across all tables synchronized with
-/// CloudKit. This means that the sum of the count of rows across all synchronized tables in your
-/// application is the number of rows this one single table holds. However, this table is held
-/// in a database separate from your app's database.
-///
-///
-@available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
-// @Table("\(String.sqliteDataCloudKitSchemaName)_metadata")
-public struct SyncMetadata: Hashable, Sendable {
-  /// The unique identifier of the record synchronized.
-  public var recordPrimaryKey: String
-
-  /// The type of the record synchronized, _i.e._ its table name.
-  public var recordType: String
-
-  /// The name of the record synchronized.
+  /// A table that tracks metadata related to synchronized data.
   ///
-  /// This field encodes both the table name and primary key of the record synchronized in
-  /// the format "primaryKey:tableName", for example:
+  /// Each row of this table represents a synchronized record across all tables synchronized with
+  /// CloudKit. This means that the sum of the count of rows across all synchronized tables in your
+  /// application is the number of rows this one single table holds. However, this table is held
+  /// in a database separate from your app's database.
   ///
-  /// ```swift
-  /// "8c4d1e4e-49b2-4f60-b6df-3c23881b87c6:reminders"
-  /// ```
-  // @Column(generated: .virtual)
-  public let recordName: String
-
-  /// The unique identifier of this record's parent, if any.
-  public var parentRecordPrimaryKey: String?
-
-  /// The type of this record's parent, _i.e._ its table name, if any.
-  public var parentRecordType: String?
-
-  /// The name of this record's parent, if any.
   ///
-  /// This field encodes both the table name and primary key of the parent record in the format
-  /// "primaryKey:tableName", for example:
-  ///
-  /// ```swift
-  /// "d35e1f81-46e4-45d1-904b-2b7df1661e3e:remindersLists"
-  /// ```
-  // @Column(generated: .virtual)
-  public let parentRecordName: String?
+  @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
+  // @Table("\(String.sqliteDataCloudKitSchemaName)_metadata")
+  public struct SyncMetadata: Hashable, Sendable {
+    /// The unique identifier of the record synchronized.
+    public var recordPrimaryKey: String
 
-  /// The last known `CKRecord` received from the server.
-  ///
-  /// This record holds only the fields that are archived when using `encodeSystemFields(with:)`.
-  // @Column(as: CKRecord?.SystemFieldsRepresentation.self)
-  public var lastKnownServerRecord: CKRecord?
+    /// The type of the record synchronized, _i.e._ its table name.
+    public var recordType: String
 
-  /// The last known `CKRecord` received from the server with all fields archived.
-  // @Column(as: CKRecord?.SystemFieldsRepresentation.self)
-  package var _lastKnownServerRecordAllFields: CKRecord?
+    /// The name of the record synchronized.
+    ///
+    /// This field encodes both the table name and primary key of the record synchronized in
+    /// the format "primaryKey:tableName", for example:
+    ///
+    /// ```swift
+    /// "8c4d1e4e-49b2-4f60-b6df-3c23881b87c6:reminders"
+    /// ```
+    // @Column(generated: .virtual)
+    public let recordName: String
 
-  /// The `CKShare` associated with this record, if it is shared.
-  // @Column(as: CKShare?.SystemFieldsRepresentation.self)
-  public var share: CKShare?
+    /// The unique identifier of this record's parent, if any.
+    public var parentRecordPrimaryKey: String?
 
-  // @Column(generated: .virtual)
-  public let isShared: Bool
+    /// The type of this record's parent, _i.e._ its table name, if any.
+    public var parentRecordType: String?
 
-  /// The date the user last modified the record.
-  public var userModificationDate: Date
+    /// The name of this record's parent, if any.
+    ///
+    /// This field encodes both the table name and primary key of the parent record in the format
+    /// "primaryKey:tableName", for example:
+    ///
+    /// ```swift
+    /// "d35e1f81-46e4-45d1-904b-2b7df1661e3e:remindersLists"
+    /// ```
+    // @Column(generated: .virtual)
+    public let parentRecordName: String?
 
-  package init(
-    recordPrimaryKey: String,
-    recordType: String,
-    parentRecordPrimaryKey: String? = nil,
-    parentRecordType: String? = nil,
-    lastKnownServerRecord: CKRecord? = nil,
-    _lastKnownServerRecordAllFields: CKRecord? = nil,
-    share: CKShare? = nil,
-    userModificationDate: Date
-  ) {
-    self.recordPrimaryKey = recordPrimaryKey
-    self.recordType = recordType
-    self.recordName = "\(recordPrimaryKey):\(recordType)"
-    self.parentRecordPrimaryKey = parentRecordPrimaryKey
-    self.parentRecordType = parentRecordType
-    if let parentRecordPrimaryKey, let parentRecordType {
-      self.parentRecordName = "\(parentRecordPrimaryKey):\(parentRecordType)"
-    } else {
-      self.parentRecordName = nil
-    }
-    self.lastKnownServerRecord = lastKnownServerRecord
-    self._lastKnownServerRecordAllFields = _lastKnownServerRecordAllFields
-    self.share = share
-    self.isShared = share != nil
-    self.userModificationDate = userModificationDate
-  }
-
-  // @Selection @Table
-  struct AncestorMetadata {
-    let recordName: String
-    let parentRecordName: String?
+    /// The last known `CKRecord` received from the server.
+    ///
+    /// This record holds only the fields that are archived when using `encodeSystemFields(with:)`.
     // @Column(as: CKRecord?.SystemFieldsRepresentation.self)
-    let lastKnownServerRecord: CKRecord?
-  }
-}
+    public var lastKnownServerRecord: CKRecord?
 
-@available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
-extension SyncMetadata {
-  package static func find<T: PrimaryKeyedTable>(
-    _ primaryKey: T.PrimaryKey.QueryOutput,
-    table _: T.Type,
-  ) -> Where<Self> {
-    Self.where {
-      SQLQueryExpression(
-        """
-        \($0.recordPrimaryKey) = \(T.PrimaryKey(queryOutput: primaryKey)) \
-        AND \($0.recordType) = \(bind: T.tableName)
-        """
-      )
+    /// The last known `CKRecord` received from the server with all fields archived.
+    // @Column(as: CKRecord?.SystemFieldsRepresentation.self)
+    package var _lastKnownServerRecordAllFields: CKRecord?
+
+    /// The `CKShare` associated with this record, if it is shared.
+    // @Column(as: CKShare?.SystemFieldsRepresentation.self)
+    public var share: CKShare?
+
+    // @Column(generated: .virtual)
+    public let isShared: Bool
+
+    /// The date the user last modified the record.
+    public var userModificationDate: Date
+
+    package init(
+      recordPrimaryKey: String,
+      recordType: String,
+      parentRecordPrimaryKey: String? = nil,
+      parentRecordType: String? = nil,
+      lastKnownServerRecord: CKRecord? = nil,
+      _lastKnownServerRecordAllFields: CKRecord? = nil,
+      share: CKShare? = nil,
+      userModificationDate: Date
+    ) {
+      self.recordPrimaryKey = recordPrimaryKey
+      self.recordType = recordType
+      self.recordName = "\(recordPrimaryKey):\(recordType)"
+      self.parentRecordPrimaryKey = parentRecordPrimaryKey
+      self.parentRecordType = parentRecordType
+      if let parentRecordPrimaryKey, let parentRecordType {
+        self.parentRecordName = "\(parentRecordPrimaryKey):\(parentRecordType)"
+      } else {
+        self.parentRecordName = nil
+      }
+      self.lastKnownServerRecord = lastKnownServerRecord
+      self._lastKnownServerRecordAllFields = _lastKnownServerRecordAllFields
+      self.share = share
+      self.isShared = share != nil
+      self.userModificationDate = userModificationDate
+    }
+
+    // @Selection @Table
+    struct AncestorMetadata {
+      let recordName: String
+      let parentRecordName: String?
+      // @Column(as: CKRecord?.SystemFieldsRepresentation.self)
+      let lastKnownServerRecord: CKRecord?
     }
   }
-}
 
-@available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
-extension PrimaryKeyedTable where PrimaryKey.QueryOutput: IdentifierStringConvertible {
-  /// Constructs a ``SyncMetadata/RecordName-swift.struct`` for a primary keyed table give an ID.
-  ///
-  /// - Parameter id: The ID of the record.
-  package static func recordName(for id: PrimaryKey.QueryOutput) -> String {
-    "\(id.rawIdentifier):\(tableName)"
+  @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
+  extension SyncMetadata {
+    package static func find<T: PrimaryKeyedTable>(
+      _ primaryKey: T.PrimaryKey.QueryOutput,
+      table _: T.Type,
+    )
+      -> Where<Self>
+    where T.PrimaryKey: IdentifierStringConvertible {
+      T.metadata(for: primaryKey)
+    }
   }
 
-  var recordName: String {
-    Self.recordName(for: self[keyPath: Self.columns.primaryKey.keyPath])
+  @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
+  extension PrimaryKeyedTable where PrimaryKey: IdentifierStringConvertible {
+    public static func metadata(for primaryKey: PrimaryKey.QueryOutput) -> Where<SyncMetadata> {
+      SyncMetadata.where {
+        SQLQueryExpression(
+          """
+          \($0.recordPrimaryKey) = \(PrimaryKey(queryOutput: primaryKey)) \
+          AND \($0.recordType) = \(bind: tableName)
+          """
+        )
+      }
+    }
   }
-}
 
-@available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
-extension PrimaryKeyedTableDefinition where PrimaryKey.QueryOutput: IdentifierStringConvertible {
-  public var recordName: some QueryExpression<String> {
-    _recordName
-  }
-}
+  @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
+  extension PrimaryKeyedTable where PrimaryKey.QueryOutput: IdentifierStringConvertible {
+    /// Constructs a ``SyncMetadata/RecordName-swift.struct`` for a primary keyed table give an ID.
+    ///
+    /// - Parameter id: The ID of the record.
+    package static func recordName(for id: PrimaryKey.QueryOutput) -> String {
+      "\(id.rawIdentifier):\(tableName)"
+    }
 
-@available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
-extension PrimaryKeyedTableDefinition {
-  var _recordName: some QueryExpression<String> {
-    SQLQueryExpression("\(primaryKey) || ':' || \(quote: QueryValue.tableName, delimiter: .text)")
+    var recordName: String {
+      Self.recordName(for: self[keyPath: Self.columns.primaryKey.keyPath])
+    }
   }
-}
+
+  @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
+  extension PrimaryKeyedTableDefinition where PrimaryKey.QueryOutput: IdentifierStringConvertible {
+    public var recordName: some QueryExpression<String> {
+      _recordName
+    }
+  }
+
+  @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
+  extension PrimaryKeyedTableDefinition {
+    var _recordName: some QueryExpression<String> {
+      SQLQueryExpression("\(primaryKey) || ':' || \(quote: QueryValue.tableName, delimiter: .text)")
+    }
+  }
 #endif
