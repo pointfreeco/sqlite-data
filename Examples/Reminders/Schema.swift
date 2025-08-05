5b23ef4f44ffd3d2d648eb9dcdd83309ceb2bc7d
larryonoff <larryonoff@gmail.com>
2025-08-05 23:36:08 +0200

    Migrate to triggers, add RemindersList Defaults (#114)
    * Migrate to triggers, add RemindersList Defaults
    
    * wip
    
    * Bump sleep for CI
    
    ---------
    
    Co-authored-by: Stephen Celis <stephen@stephencelis.com>

--- Examples/Reminders/Schema.swift
+++ Examples/Reminders/Schema.swift
@@ -9,9 +9,12 @@ import SwiftUI
 struct RemindersList: Hashable, Identifiable {
   let id: UUID
   @Column(as: Color.HexRepresentation.self)
-  var color = Color(red: 0x4a / 255, green: 0x99 / 255, blue: 0xef / 255)
+  var color: Color = Self.defaultColor
   var position = 0
   var title = ""
+
+  static var defaultColor: Color { Color(red: 0x4a / 255, green: 0x99 / 255, blue: 0xef / 255) }
+  static var defaultTitle: String { "Personal" }
 }
 
 extension RemindersList.Draft: Identifiable {}
@@ -123,11 +126,12 @@ func appDatabase() throws -> any DatabaseWriter {
     migrator.eraseDatabaseOnSchemaChange = true
   #endif
   migrator.registerMigration("Create initial tables") { db in
+    let defaultListColor = Color.HexRepresentation(queryOutput: RemindersList.defaultColor).hexValue
     try #sql(
       """
       CREATE TABLE "remindersLists" (
         "id" TEXT PRIMARY KEY NOT NULL ON CONFLICT REPLACE DEFAULT (uuid()),
-        "color" INTEGER NOT NULL DEFAULT \(raw: 0x4a99_ef00),
+        "color" INTEGER NOT NULL DEFAULT \(raw: defaultListColor ?? 0),
         "position" INTEGER NOT NULL DEFAULT 0,
         "title" TEXT NOT NULL
       ) STRICT
@@ -178,49 +182,33 @@ func appDatabase() throws -> any DatabaseWriter {
 
   try migrator.migrate(database)
 
-  if context == .preview {
-    try database.write { db in
+  try database.write { db in
+    if context == .preview {
       try db.seedSampleData()
     }
-  }
 
-  try database.write { db in
-    try #sql(
-      """
-      CREATE TEMPORARY TRIGGER "default_position_reminders_lists" 
-      AFTER INSERT ON "remindersLists"
-      FOR EACH ROW BEGIN
-        UPDATE "remindersLists"
-        SET "position" = (SELECT max("position") + 1 FROM "remindersLists")
-        WHERE "id" = NEW."id";
-      END
-      """
-    )
+    try RemindersList.createTemporaryTrigger(after: .insert { new in
+      RemindersList
+        .find(new.id)
+        .update { $0.position = RemindersList.select { ($0.position.max() ?? -1) + 1} }
+    })
     .execute(db)
-    try #sql(
-      """
-      CREATE TEMPORARY TRIGGER "default_position_reminders" 
-      AFTER INSERT ON "reminders"
-      FOR EACH ROW BEGIN
-        UPDATE "reminders"
-        SET "position" = (SELECT max("position") + 1 FROM "reminders")
-        WHERE "id" = NEW."id";
-      END
-      """
-    )
+    try Reminder.createTemporaryTrigger(after: .insert { new in
+      Reminder
+        .find(new.id)
+        .update { $0.position = Reminder.select { ($0.position.max() ?? -1) + 1} }
+    })
     .execute(db)
-    try #sql(
-      """
-      CREATE TEMPORARY TRIGGER "non_empty_reminders_lists" 
-      AFTER DELETE ON "remindersLists"
-      FOR EACH ROW BEGIN
-        INSERT INTO "remindersLists"
-        ("title", "color")
-        SELECT 'Personal', \(raw: 0x4a99ef)
-        WHERE (SELECT count(*) FROM "remindersLists") = 0;
-      END
-      """
-    )
+    try RemindersList.createTemporaryTrigger(after: .delete { _ in
+      RemindersList.insert {
+        RemindersList.Draft(
+          color: RemindersList.defaultColor,
+          title: RemindersList.defaultTitle
+        )
+      }
+    } when: { _ in
+      !RemindersList.exists()
+    })
     .execute(db)
   }
 
