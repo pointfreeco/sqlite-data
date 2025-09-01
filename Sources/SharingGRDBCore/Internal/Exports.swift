e95e09d26b2cad092741d1100b5e1a949af68f1a
Stephen Celis <stephen@stephencelis.com>
2025-09-01 14:11:09 -0700

    Minimize exports

--- Sources/SharingGRDBCore/Internal/Exports.swift
+++ Sources/SharingGRDBCore/Internal/Exports.swift
@@ -1,4 +1,12 @@
 @_exported import Dependencies
-@_exported import GRDB
 @_exported import Sharing
 @_exported import StructuredQueriesGRDBCore
+
+@_exported import class GRDB.Database
+@_exported import class GRDB.DatabasePool
+@_exported import class GRDB.DatabaseQueue
+@_exported import protocol GRDB.DatabaseReader
+@_exported import protocol GRDB.DatabaseWriter
+@_exported import protocol GRDB.ValueObservationScheduler
+@_exported import struct GRDB.Configuration
+@_exported import struct GRDB.DatabaseMigrator
