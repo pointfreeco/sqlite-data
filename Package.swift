d84ca0dfececa29c0a2ce574af482ccc9693629b
Stephen Celis <stephen@stephencelis.com>
2025-09-02 12:06:09 -0700

    StructuredQueries 0.16 support (#150)
    Just a minor change to the Reminders demo, which defined a custom query
    representation.

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.5.0"),
     .package(url: "https://github.com/pointfreeco/swift-sharing", from: "2.3.0"),
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.4"),
-    .package(url: "https://github.com/pointfreeco/swift-structured-queries", from: "0.15.0"),
+    .package(url: "https://github.com/pointfreeco/swift-structured-queries", from: "0.16.0"),
   ],
   targets: [
     .target(
