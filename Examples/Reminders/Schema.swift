e5abc8a9ddae18239afd7e8cff5ebb2806bbcec2
Stephen Celis <stephen@stephencelis.com>
2025-09-27 23:29:18 -0700

    wip

--- Examples/Reminders/Schema.swift
+++ Examples/Reminders/Schema.swift
@@ -102,32 +102,9 @@ extension Tag {
 
 @Table("remindersTags")
 struct ReminderTag: Identifiable {
-  // @Columns
-  // let id: ReminderTagID
   let id: UUID
   let reminderID: Reminder.ID
   let tagID: Tag.ID
-}
-
-@Table
-struct ReminderTagID: Hashable {
-  let reminderID: Reminder.ID
-  let tagID: Tag.ID
-}
-
-extension ReminderTagID: IdentifierStringConvertible {
-  init?(rawIdentifier: String) {
-    let segments = rawIdentifier.split(separator: "/", maxSplits: 1)
-    guard
-      segments.count == 2,
-      let reminderID = Reminder.ID(uuidString: String(segments[0]))
-    else { return nil }
-    self.init(reminderID: reminderID, tagID: Tag.ID(segments[1]))
-  }
-
-  var rawIdentifier: String {
-    "\(reminderID)/\(tagID)"
-  }
 }
 
 @Table
