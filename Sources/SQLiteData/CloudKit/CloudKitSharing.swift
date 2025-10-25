56f2d09267411007bb58d66a930b0f33096068a4
Brandon Williams <mbrandonw@hey.com>
2025-10-24 22:37:51 -0500

    Fix bug that reset timestamp of record when sharing.

--- Sources/SQLiteData/CloudKit/CloudKitSharing.swift
+++ Sources/SQLiteData/CloudKit/CloudKitSharing.swift
@@ -137,12 +137,11 @@
         )
       }
 
-      let rootRecord =
-        lastKnownServerRecord
-        ?? CKRecord(
-          recordType: recordType,
-          recordID: CKRecord.ID(recordName: recordName, zoneID: defaultZone.zoneID)
-        )
+      let rootRecordID =
+      lastKnownServerRecord?.recordID
+      ?? CKRecord.ID(recordName: recordName, zoneID: defaultZone.zoneID)
+      let cloudKitDatabase = container.database(for: rootRecordID)
+      let rootRecord = try await cloudKitDatabase.record(for: rootRecordID)
 
       var existingShare: CKShare? {
         get async throws {
@@ -157,8 +156,7 @@
             return nil
           }
           do {
-            return try await container.database(for: rootRecord.recordID)
-              .record(for: shareRecordID) as? CKShare
+            return try await cloudKitDatabase.record(for: shareRecordID) as? CKShare
           } catch let error as CKError where error.code == .unknownItem {
             return nil
           }
