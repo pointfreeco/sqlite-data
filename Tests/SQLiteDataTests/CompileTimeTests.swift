b66b894b9a5710f1072c8eb6448a7edfc2d743d9
Stephen Celis <stephen@stephencelis.com>
2025-10-30 11:36:21 -0700

    Fix issues with `CK{Record,Share}.SystemFieldsRepresentation` (#284)
    * Fix issues with `CK{Record,Share}.SystemFieldsRepresentation`
    
    I'm a little surprised that this diff fixes things, but it does locally
    for me, in the latest Xcode 26, so let's test this change a bit before
    merging.
    
    * Compile time tests
    
    * wip;
    
    * wip
    
    ---------
    
    Co-authored-by: Brandon Williams <mbrandonw@hey.com>

--- Tests/SQLiteDataTests/CompileTimeTests.swift
+++ Tests/SQLiteDataTests/CompileTimeTests.swift
@@ -1,5 +1,9 @@
 import SQLiteData
 
+#if canImport(CloudKit)
+  import CloudKit
+#endif
+
 private final class Model {
   @FetchAll var titles: [String]
 
@@ -7,3 +11,33 @@ private final class Model {
     _titles = FetchAll(Reminder.select(\.title))
   }
 }
+
+#if canImport(CloudKit)
+  @Table
+  struct RepresentableFields {
+    @Column(as: CKShare.SystemFieldsRepresentation.self)
+    var share: CKShare
+    @Column(as: CKShare?.SystemFieldsRepresentation.self)
+    var optionalShare: CKShare?
+    @Column(as: CKRecord.SystemFieldsRepresentation.self)
+    var record: CKRecord
+    @Column(as: CKRecord?.SystemFieldsRepresentation.self)
+    var optionalRecord: CKRecord?
+  }
+
+  @DatabaseFunction(
+    as: ((
+      CKShare.SystemFieldsRepresentation,
+      CKRecord.SystemFieldsRepresentation,
+      CKShare?.SystemFieldsRepresentation,
+      CKRecord?.SystemFieldsRepresentation
+    ) -> Void).self
+  )
+  nonisolated func representableArguments(
+    share: CKShare,
+    record: CKRecord,
+    optionalShare: CKShare?,
+    optionalRecord: CKRecord?
+  ) {
+  }
+#endif
