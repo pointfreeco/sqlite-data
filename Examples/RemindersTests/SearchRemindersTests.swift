76b33eaeaeb8a3e2ef4a51e3e9ee63e6c841f991
Brandon Williams <mbrandonw@hey.com>
2025-11-17 14:39:40 -0600

    wip

--- Examples/RemindersTests/SearchRemindersTests.swift
+++ Examples/RemindersTests/SearchRemindersTests.swift
@@ -8,9 +8,9 @@ import Testing
 
 extension BaseTestSuite {
   @MainActor
-  @Suite(
-    .snapshots(record: .missing)
-  )
+  //  @Suite(
+  //    .snapshots(record: .missing)
+  //  )
   struct SearchRemindersTests {
     @Dependency(\.defaultDatabase) var database
 
@@ -28,38 +28,34 @@ extension BaseTestSuite {
       model.searchText = "Take"
       try await model.searchTask?.value
       #expect(model.searchResults.completedCount == 1)
-      withKnownIssue(
-        "'@Fetch' introduces an escaping closure and loses the task-local dependency"
-      ) {
-        assertInlineSnapshot(of: model.searchResults.rows, as: .customDump) {
-          """
-          [
-            [0]: SearchRemindersModel.Row(
-              isPastDue: false,
+      assertInlineSnapshot(of: model.searchResults.rows, as: .customDump) {
+        """
+        [
+          [0]: SearchRemindersModel.Row(
+            isPastDue: true,
+            notes: "",
+            reminder: Reminder(
+              id: UUID(00000000-0000-0000-0000-00000000000A),
+              dueDate: Date(2009-02-17T23:31:30.000Z),
+              isFlagged: false,
               notes: "",
-              reminder: Reminder(
-                id: UUID(00000000-0000-0000-0000-00000000000A),
-                dueDate: Date(2009-02-17T23:31:30.000Z),
-                isCompleted: false,
-                isFlagged: false,
-                notes: "",
-                position: 8,
-                priority: .high,
-                remindersListID: UUID(00000000-0000-0000-0000-000000000001),
-                title: "Take out trash"
-              ),
-              remindersList: RemindersList(
-                id: UUID(00000000-0000-0000-0000-000000000001),
-                color: 3985191935,
-                position: 2,
-                title: "Family"
-              ),
-              tags: "",
-              title: "**Take** out trash"
-            )
-          ]
-          """
-        }
+              position: 8,
+              priority: .high,
+              remindersListID: UUID(00000000-0000-0000-0000-000000000001),
+              status: .incomplete,
+              title: "Take out trash"
+            ),
+            remindersList: RemindersList(
+              id: UUID(00000000-0000-0000-0000-000000000001),
+              color: 3985191935,
+              position: 2,
+              title: "Family"
+            ),
+            tags: "",
+            title: "**Take** out trash"
+          )
+        ]
+        """
       }
     }
 
@@ -68,61 +64,10 @@ extension BaseTestSuite {
       model.searchText = "Take"
       try await model.showCompletedButtonTapped()
 
-      withKnownIssue(
-        "'@Fetch' introduces an escaping closure and loses the task-local dependency"
-      ) {
-        assertInlineSnapshot(of: model.searchResults.rows, as: .customDump) {
-          """
-          [
-            [0]: SearchRemindersModel.Row(
-              isPastDue: false,
-              notes: "",
-              reminder: Reminder(
-                id: UUID(00000000-0000-0000-0000-00000000000A),
-                dueDate: Date(2009-02-17T23:31:30.000Z),
-                isCompleted: false,
-                isFlagged: false,
-                notes: "",
-                position: 8,
-                priority: .high,
-                remindersListID: UUID(00000000-0000-0000-0000-000000000001),
-                title: "Take out trash"
-              ),
-              remindersList: RemindersList(
-                id: UUID(00000000-0000-0000-0000-000000000001),
-                color: 3985191935,
-                position: 2,
-                title: "Family"
-              ),
-              tags: "",
-              title: "**Take** out trash"
-            ),
-            [1]: SearchRemindersModel.Row(
-              isPastDue: false,
-              notes: "",
-              reminder: Reminder(
-                id: UUID(00000000-0000-0000-0000-000000000006),
-                dueDate: Date(2008-08-07T23:31:30.000Z),
-                isCompleted: true,
-                isFlagged: false,
-                notes: "",
-                position: 4,
-                priority: nil,
-                remindersListID: UUID(00000000-0000-0000-0000-000000000000),
-                title: "Take a walk"
-              ),
-              remindersList: RemindersList(
-                id: UUID(00000000-0000-0000-0000-000000000000),
-                color: 1218047999,
-                position: 1,
-                title: "Personal"
-              ),
-              tags: "#car #kids #social",
-              title: "**Take** a walk"
-            )
-          ]
-          """
-        }
+      assertInlineSnapshot(of: model.searchResults.rows, as: .customDump) {
+        """
+        []
+        """
       }
     }
 
@@ -133,38 +78,10 @@ extension BaseTestSuite {
       model.deleteCompletedReminders()
       try await model.$searchResults.load()
       #expect(model.searchResults.completedCount == 0)
-      withKnownIssue(
-        "'@Fetch' introduces an escaping closure and loses the task-local dependency"
-      ) {
-        assertInlineSnapshot(of: model.searchResults.rows, as: .customDump) {
-          """
-          [
-            [0]: SearchRemindersModel.Row(
-              isPastDue: false,
-              notes: "",
-              reminder: Reminder(
-                id: UUID(00000000-0000-0000-0000-00000000000A),
-                dueDate: Date(2009-02-17T23:31:30.000Z),
-                isCompleted: false,
-                isFlagged: false,
-                notes: "",
-                position: 8,
-                priority: .high,
-                remindersListID: UUID(00000000-0000-0000-0000-000000000001),
-                title: "Take out trash"
-              ),
-              remindersList: RemindersList(
-                id: UUID(00000000-0000-0000-0000-000000000001),
-                color: 3985191935,
-                position: 2,
-                title: "Family"
-              ),
-              tags: "",
-              title: "**Take** out trash"
-            )
-          ]
-          """
-        }
+      assertInlineSnapshot(of: model.searchResults.rows, as: .customDump) {
+        """
+        []
+        """
       }
     }
   }
