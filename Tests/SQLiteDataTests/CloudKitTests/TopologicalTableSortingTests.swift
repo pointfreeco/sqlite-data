281ec18ff27f9c6786af5a1bfd9cbf7ca8c03b77
Brandon Williams <mbrandonw@hey.com>
2026-01-06 11:54:15 -0600

    clean up

--- Tests/SQLiteDataTests/CloudKitTests/TopologicalTableSortingTests.swift
+++ Tests/SQLiteDataTests/CloudKitTests/TopologicalTableSortingTests.swift
@@ -9,7 +9,20 @@
       @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
       @Test func tablesByOrder() async throws {
         #expect(
-          syncEngine.tablesByOrder == ["remindersListPrivates": 11, "childWithOnDeleteSetNulls": 6, "reminders": 1, "modelAs": 8, "remindersLists": 0, "reminderTags": 4, "modelBs": 9, "parents": 5, "childWithOnDeleteSetDefaults": 7, "modelCs": 10, "remindersListAssets": 2, "tags": 3]
+          syncEngine.tablesByOrder == [
+            "remindersLists": 0,
+            "reminders": 1,
+            "remindersListAssets": 2,
+            "tags": 3,
+            "reminderTags": 4,
+            "parents": 5,
+            "childWithOnDeleteSetNulls": 6,
+            "childWithOnDeleteSetDefaults": 7,
+            "modelAs": 8,
+            "modelBs": 9,
+            "modelCs": 10,
+            "remindersListPrivates": 11,
+          ]
         )
       }
     }
