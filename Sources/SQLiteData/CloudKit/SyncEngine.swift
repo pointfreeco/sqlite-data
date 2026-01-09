105a55fbfb949228e34e838523aa8885ca54915e
Stephen Celis <stephen@stephencelis.com>
2026-01-09 12:19:03 -0800

    wip

--- Sources/SQLiteData/CloudKit/SyncEngine.swift
+++ Sources/SQLiteData/CloudKit/SyncEngine.swift
@@ -13,6 +13,8 @@
     import UIKit
   #endif
 
+extension String: Error {}
+
   /// An object that manages the synchronization of local and remote SQLite data.
   ///
   /// See <doc:CloudKit> for more information.
@@ -99,10 +101,11 @@
       repeat (each T2).PrimaryKey.QueryOutput: IdentifierStringConvertible,
       repeat (each T2).TableColumns.PrimaryColumn: WritableTableColumnExpression
     {
+      @Dependency(\.context) var context
       let containerIdentifier =
         containerIdentifier
         ?? ModelConfiguration(groupContainer: .automatic).cloudKitContainerIdentifier
-
+        (context == .preview ? "preview" : nil)
       var allTables: [any SynchronizableTable] = []
       var allPrivateTables: [any SynchronizableTable] = []
       for table in repeat each tables {
@@ -113,7 +116,6 @@
       }
       let userDatabase = UserDatabase(database: database)
 
-      @Dependency(\.context) var context
       guard context == .live
       else {
         let privateDatabase = MockCloudDatabase(databaseScope: .private)
@@ -325,7 +327,10 @@
           : URL(filePath: metadatabase.path).lastPathComponent
         let attachedMetadatabaseName =
           URL(string: attachedMetadatabasePath)?.lastPathComponent ?? ""
-        if metadatabaseName != attachedMetadatabaseName {
+        @Dependency(\.context) var context
+        if metadatabaseName != attachedMetadatabaseName
+          && !(context == .preview && attachedMetadatabaseName.isEmpty)
+        {
           throw SchemaError(
             reason: .metadatabaseMismatch(
               attachedPath: attachedMetadatabasePath,
@@ -337,7 +342,6 @@
               """
           )
         }
-
       } else {
         try #sql(
           """
@@ -2151,9 +2155,11 @@
     /// - Parameter containerIdentifier: The identifier of the CloudKit container used to
     /// synchronize data. Defaults to the value set in the app's entitlements.
     public func attachMetadatabase(containerIdentifier: String? = nil) throws {
+      @Dependency(\.context) var context
       let containerIdentifier =
         containerIdentifier
         ?? ModelConfiguration(groupContainer: .automatic).cloudKitContainerIdentifier
+        ?? (context == .preview ? "preview" : nil)
 
       guard let containerIdentifier else {
         throw SyncEngine.SchemaError.noCloudKitContainer
