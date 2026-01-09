439cd82a6a254f8457e932ff800cbc5bc31f83bd
Brandon Williams <mbrandonw@hey.com>
2026-01-08 18:17:55 -0600

    wip

--- Tests/SQLiteDataTests/CloudKitTests/SharingTests.swift
+++ Tests/SQLiteDataTests/CloudKitTests/SharingTests.swift
@@ -851,6 +851,7 @@
        * Create parent record and synchronize.
        * Create child record and synchronize.
        * Share parent record.
+       * See: https://github.com/pointfreeco/sqlite-data/pull/363
        */
       @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
       @Test func createParentThenChildThenShare() async throws {
