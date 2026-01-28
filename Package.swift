71070ef9a7c47c5f1ba24fe6212ca561a156427c
Stephen Celis <stephen@stephencelis.com>
2026-01-28 11:43:04 -0800

    wip

--- Package.swift
+++ Package.swift
@@ -37,8 +37,7 @@ let package = Package(
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.4"),
     .package(
       url: "https://github.com/pointfreeco/swift-structured-queries",
-      branch: "main",
-      // from: "0.27.0",
+      from: "0.29.0",
       traits: [
         .trait(name: "StructuredQueriesTagged", condition: .when(traits: ["SQLiteDataTagged"]))
       ]
