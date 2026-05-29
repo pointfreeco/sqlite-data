a5e6cc91a3e8ae5ec0d51b6942d359386711291b
Brandon Williams <mbrandonw@hey.com>
2026-05-29 11:57:13 -0500

    wip

--- Sources/SQLiteData/FetchOne.swift
+++ Sources/SQLiteData/FetchOne.swift
@@ -84,13 +84,13 @@ public struct FetchOne<Value: Sendable>: Sendable {
   /// Initializes this property with a wrapped value.
   ///
   /// - Parameter wrappedValue: A default value to associate with this property.
-//  public init(wrappedValue: sending Value)
-//  where
-//    Value: _Selection,
-//    Value.QueryOutput == Value
-//  {
-//    sharedReader = SharedReader(value: wrappedValue)
-//  }
+  public init(wrappedValue: sending Value)
+  where
+    Value: _Selection,
+    Value.QueryOutput == Value
+  {
+    sharedReader = SharedReader(value: wrappedValue)
+  }
 
   /// Initializes this property with a query that fetches the first row from a table.
   ///
