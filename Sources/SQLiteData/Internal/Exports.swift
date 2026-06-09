989de9688ead0d4143fe1cc9a58c82b157d70094
Stephen Celis <stephen@stephencelis.com>
2026-06-09 11:54:40 -0700

    wip

--- Sources/SQLiteData/Internal/Exports.swift
+++ Sources/SQLiteData/Internal/Exports.swift
@@ -1,15 +1,15 @@
 #if !EXCLUDE_EXPORTS
   @_exported import Dependencies
   @_exported import StructuredQueriesSQLite
-
-  public import GRDB
-  public typealias Configuration = GRDB.Configuration
-  public typealias Database = GRDB.Database
-  public typealias DatabaseError = GRDB.DatabaseError
-  public typealias DatabaseMigrator = GRDB.DatabaseMigrator
-  public typealias DatabasePool = GRDB.DatabasePool
-  public typealias DatabaseQueue = GRDB.DatabaseQueue
-  public typealias DatabaseReader = GRDB.DatabaseReader
-  public typealias DatabaseWriter = GRDB.DatabaseWriter
-  public typealias ValueObservationScheduler = GRDB.ValueObservationScheduler
 #endif
+
+public import GRDB
+public typealias Configuration = GRDB.Configuration
+public typealias Database = GRDB.Database
+public typealias DatabaseError = GRDB.DatabaseError
+public typealias DatabaseMigrator = GRDB.DatabaseMigrator
+public typealias DatabasePool = GRDB.DatabasePool
+public typealias DatabaseQueue = GRDB.DatabaseQueue
+public typealias DatabaseReader = GRDB.DatabaseReader
+public typealias DatabaseWriter = GRDB.DatabaseWriter
+public typealias ValueObservationScheduler = GRDB.ValueObservationScheduler
