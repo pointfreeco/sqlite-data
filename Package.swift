f58622ec96f796302b278ff4e8ce62c2c8323eca
Stephen Celis <stephen@stephencelis.com>
2026-06-29 10:57:45 -0700

    Add `CasePaths` and `Tagged` traits (#479)
    * Add `CasePaths` and `Tagged` traits
    
    The existing `SQLiteDataTagged` trait will serve as a deprecated alias
    to `Tagged`, while `CasePaths` will allow SQLiteData users to enable
    enum tables without an explicit dependency on
    `swift-structured-queries`.
    
    * wip
    
    * wip

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version: 6.1
 
+import Foundation
 import PackageDescription
 
 let package = Package(
@@ -22,9 +23,18 @@ let package = Package(
   ],
   traits: [
     .trait(
-      name: "SQLiteDataTagged",
+      name: "CasePaths",
+      description: "Introduce support for enum tables."
+    ),
+    .trait(
+      name: "Tagged",
       description: "Introduce SQLiteData conformances to the swift-tagged package."
-    )
+    ),
+    .trait(
+      name: "SQLiteDataTagged",
+      description: "A deprecated alias for the 'Tagged' trait.",
+      enabledTraits: ["Tagged"]
+    ),
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-collections", from: "1.0.0"),
@@ -37,9 +47,10 @@ let package = Package(
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.4"),
     .package(
       url: "https://github.com/pointfreeco/swift-structured-queries",
-      from: "0.31.0",
+      from: "0.32.0",
       traits: [
-        .trait(name: "StructuredQueriesTagged", condition: .when(traits: ["SQLiteDataTagged"]))
+        .trait(name: "CasePaths", condition: .when(traits: ["CasePaths"])),
+        .trait(name: "Tagged", condition: .when(traits: ["Tagged"])),
       ]
     ),
     .package(url: "https://github.com/pointfreeco/swift-tagged", from: "0.10.0"),
@@ -60,7 +71,7 @@ let package = Package(
         .product(
           name: "Tagged",
           package: "swift-tagged",
-          condition: .when(traits: ["SQLiteDataTagged"])
+          condition: .when(traits: ["Tagged"])
         ),
       ]
     ),
@@ -108,6 +119,9 @@ for target in package.targets {
       .enableUpcomingFeature("InternalImportsByDefault"),
       .enableUpcomingFeature("MemberImportVisibility"),
     ])
+    if ProcessInfo.processInfo.environment.keys.contains("EXCLUDE_EXPORTS") {
+      target.swiftSettings?.append(.define("EXCLUDE_EXPORTS"))
+    }
   }
 }
 
