15eb4b72a75d93bc2d7fe22a672694cf3919d8a1
Stephen Celis <stephen@stephencelis.com>
2026-08-27 15:18:45 -0700

    Bump to IssueReporting 2.0 (#491)
    * Bump to IssueReporting 2.0
    
    * wip

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -29,7 +29,7 @@ let package = Package(
     .package(url: "https://github.com/pointfreeco/swift-sharing", from: "2.3.0"),
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.4"),
     .package(url: "https://github.com/pointfreeco/swift-structured-queries", from: "0.35.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.5.0"),
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.13.0"),
   ],
   targets: [
     .target(
