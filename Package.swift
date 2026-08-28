8658868f23d695353bbf0ef006fa6cba9e60dbd0
Stephen Celis <stephen@stephencelis.com>
2026-08-28 16:45:54 -0700

    StructuredQueries 0.38 support
    StructuredQueries protocol requirement that we need to further weaken
    here.
    
    Fixes #539.

--- Package.swift
+++ Package.swift
@@ -70,7 +70,7 @@ let package = Package(
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.4"),
     .package(
       url: "https://github.com/pointfreeco/swift-structured-queries",
-      from: "0.36.0",
+      from: "0.38.1",
       traits: [
         .trait(name: "CasePaths", condition: .when(traits: ["CasePaths"])),
         .trait(name: "ColumnCoding", condition: .when(traits: ["ColumnCoding"])),
